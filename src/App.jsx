26d1b528bd75de979c1e324bf204e3d4cc348fb6
hafiz's laptop <hafizshukor03@gmail.com>
2024-01-17 17:08:36 +0800

    add .jsx to the src file and add @ in vite config

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
-import DataList from './component/dataList'
+import DataList from './component/DataList.jsx'
 
 
 
--- src/component/DataList.jsx
+++ src/component/DataList.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import axios from "axios";
+
 
 const DataList = ({ listOfItem }) => {
   const [search, setSearch] = useState("");
