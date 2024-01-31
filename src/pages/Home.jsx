e0d7ea86aa2ce2cf362b4d5bfa68412b0133b8fb
hafiz's laptop <hafizshukor03@gmail.com>
2024-01-31 17:07:29 +0800

    changed path on the Home.jsx for useStore

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,6 @@
 // Home.jsx
 import FetchData from "../component/FetchData.jsx";
-import useStore from "../component/Store.jsx";
+import useStore from "../component/store";
 import Card from "../component/Card";
 import CategoryFilter from "../component/CategoryFilter";
 import { useState } from "react";
